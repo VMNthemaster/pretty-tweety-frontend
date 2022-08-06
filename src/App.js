46d2391cfa82191920a56f91b65fd4f1c28acb6a
VMNthemaster <varad.nadkarni09@gmail.com>
2022-08-06 11:35:04 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from 'axios'
+import React from "react";
 
 function App() {
+  const [file, setFile] = useState('')
+  const [fileUrl, setFileUrl] = useState("")
+  const handleSubmit = async (e) => {
+    console.log(file, typeof file)
+    e.preventDefault()
+    const formData = new FormData()
+    formData.append("image", file)
+    const res = await axios.post('http://localhost:5000/api/bg/postbgimage', formData, 
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+      
+    }
+    )
+    console.log(res)
+    setFileUrl(res.data.newBackgroundImage.awsUrl)
+  } 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="file" accept="image/*" name="image" onChange={(e) => setFile(e.target.files[0])} placeholder="Enter background image" />
+        <button type="submit">Submit</button>
+
+      {fileUrl && <img src={fileUrl} alt="bgimage" />}
+
+      </form>
     </div>
   );
 }
